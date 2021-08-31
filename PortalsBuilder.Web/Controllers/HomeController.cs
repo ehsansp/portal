8e3e1bf18d2e0ff23ddd679bfcef13117ae4a8a5
Majid <Majid@DESKTOP-VAP904G>
2021-08-31 16:15:34 +0430

    123

---
+++ PortalsBuilder.Web/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PortalsBuilder.Web.Models;
+
+namespace PortalsBuilder.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("about")]
+        public IActionResult about()
+        {
+            return View();
+        }
+        [Route("team")]
+        public IActionResult team()
+        {
+            return View();
+        }
+        [Route("contact")]
+        public IActionResult contact()
+        {
+            return View();
+        }
+        [Route("faq")]
+        public IActionResult faq()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
