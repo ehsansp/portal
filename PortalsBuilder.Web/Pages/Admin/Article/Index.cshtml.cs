4a58786806a85e90f2ee72b35f09b4a640889612
SaeidPour, Ehsan (TEK) <SaeidPour.Ehsan@Okala.com>
2021-09-06 19:35:58 +0430

    New Article

--- PortalsBuilder.Web/Pages/Admin/Article/Index.cshtml.cs
+++ PortalsBuilder.Web/Pages/Admin/Article/Index.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortalBuilder.Core.DTOs.Article;
+using PortalBuilder.Core.Services.Interfaces;
 
 namespace PortalsBuilder.Web.Pages.Admin.Article
 {
     public class IndexModel : PageModel
     {
+        private IArticleService _articleService;
+
+        public IndexModel(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        public List<ShowArticleForAdminViewModel> ListArticle { get; set; }
         public void OnGet()
         {
+            ListArticle = _articleService.GetArticlesForAdmin();
         }
     }
 }
